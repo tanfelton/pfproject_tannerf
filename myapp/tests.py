e18eeb298b55a5425513637daf53f031306a1ba1
tannerfelton <tanner.felton@xatorcorp.com>
2023-05-02 14:15:58 -0400

    add test

--- myapp/tests.py
+++ myapp/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from .models import Book
+from myapp import views
 
-# Create your tests here.
+
+        
+class ViewsTestCase(TestCase):
+    def test_index_loads_properly(self):
+        """The index page loads properly"""
+        response = self.client.get('your_server_ip:8000')
+        self.assertEqual(response.status_code, 200)
